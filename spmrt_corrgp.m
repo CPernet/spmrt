502e6a79ca7543e2a0fad7361c7a5cab120cc8eb
Cyril Pernet <wamcyril@gmail.com>
2017-03-23 23:39:55 +0000

    corrgp - fixed varous typos and stats

--- spmrt_corrgp.m
+++ spmrt_corrgp.m
@@ -1,7 +1,7 @@
 function [W,B,Maps,Stats] = spmrt_corrgp(series1,series2,masks,threshold,covariate,figout)
 
 % Routine that allows to test for image reliability based on correlations. 
-% The function calls iteratively spmup_corr to compute the Pearson and
+% The function calls iteratively spmrt_corr to compute the Pearson and
 % concordance correlations between images from series1 and series2
 % All pairs are computed to produce within vs. between pairs estimates
 % For instance for series1 = [A1 A2 A3] and series2 = [B1 B2 B3],
@@ -12,16 +12,20 @@ function [W,B,Maps,Stats] = spmrt_corrgp(series1,series2,masks,threshold,covaria
 %
 % FORMAT = [W,B,Stats] = spmup_corrgp(series1,series2,masks,figout,threshold)
 %
-% INPUT series1 series of image filename (see spm_select)
-%       series2 series of image filename (see spm_select)
-%       masks series of image filename (see spm_select)
+% INPUT series1 series of image filename (see spm_select - e.g. A1 A2 A3)
+%       series2 series of image filename (see spm_select  - e.g. B1 B2 B3)
+%       masks series of image filename (see spm_select - e.g. M1 M2 M3)
+%           note it is expected that A1 B1 and M1 are from the same subject
 %       figout 1/0 (default) to get all correlation figures out
 %       threshold (optional) if masks are not binary, threshold to apply
 %
 % output W are the within pairs correlations (Pearson and Concordance)
 %        B are the within pairs correlations (Pearson and Concordance)
 %        Maps filenames of the within and between subject maps
-%        Stats is structure
+%        Stats is structure with mean and medians (with percentille
+%        bootstrap 95% CI - to read column-wise) of within, and between 
+%        Pearson and Concordance correlation ; as well as the difference 
+%        in means and medians. P-values 
 %
 % Cyril Pernet
 % --------------------------------------------------------------------------
@@ -39,14 +43,14 @@ end
 
 if size(masks,1) == 1
     disp('only one mask used for all images')
-    masks = repmat(mask,size(series1,1),1);
+    masks = repmat(masks,size(series1,1),1);
 end
 
 %% do the within loop
 N = size(series1,1); W = nan(N,2); 
 for n=1:N
     fprintf('Computing within pairs correlations: pair %g/%g \n',n,N)
-    [W(n,1),W(n,2)]=spmup_corr(series1(n,:),series2(n,:),masks(n,:),figout,threshold);
+    [W(n,1),W(n,2)]=spmrt_corr(series1(n,:),series2(n,:),masks(n,:),figout,threshold);
 end
 
 %% make map
@@ -54,12 +58,11 @@ if nargout == 3
     img1 = spm_read_vols(spm_vol(series1(1,:)));
     img2 = spm_read_vols(spm_vol(series2(1,:)));
     for n=2:N
-        fprintf('Computing within pairs map: pair %g/%g \n',n,N)
         img1 = img1+spm_read_vols(spm_vol(series1(n,:)));
         img2 = img2+spm_read_vols(spm_vol(series2(n,:)));
     end
     img1 = img1./N; img2 = img2./N; Wimg = img1-img2;
-    WW = spm_vol(series1(1,n)); WW.fname = [pwd filesep 'within_pairs_difference.nii'];
+    WW = spm_vol(series1(1,:)); WW.fname = [pwd filesep 'within_pairs_difference.nii'];
     Maps{1} = spm_write_vol(WW,Wimg); clear WW Wimg img1 img2
 end
 
@@ -68,8 +71,8 @@ combinations = nchoosek([1:size(series1,1)],2);
 MP = NaN(N,N); MC = NaN(N,N); 
 for n=1:length(combinations)
     fprintf('Computing between pairs correlations: pair %g/%g \n',n,length(combinations))    
-    [MP(combinations(n,1),combinations(n,2)),MC(combinations(n,1),combinations(n,2))] = spmup_corr(...
-        series1(combinations(n,1),:),series2(combinations(n,2),:),masks(n,:),figout,threshold);
+    [MP(combinations(n,1),combinations(n,2)),MC(combinations(n,1),combinations(n,2))] = spmrt_corr(...
+        series1(combinations(n,1),:),series2(combinations(n,2),:),masks(combinations(n,1),:),figout,threshold);
     MP(combinations(n,2),combinations(n,1)) = MP(combinations(n,1),combinations(n,2));
     MC(combinations(n,2),combinations(n,1)) = MC(combinations(n,1),combinations(n,2));
 end
@@ -79,7 +82,6 @@ B = [nanmean(MP,2) nanmean(MC,2)];
 %% make map
 if nargout == 3
     for n=1:N
-        fprintf('Computing between pairs map: pair %g/%g \n',n,N))
         img = spm_read_vols(spm_vol(series1(n,:)));
         if n == 1
             Bimg = zeros(size(img));
@@ -89,10 +91,10 @@ if nargout == 3
         for d=1:length(m)
             img = img + spm_read_vols(spm_vol(series1(m(d),:)));
         end
-        Bimg = Bing + img./n;
+        Bimg = Bimg + img./n;
     end
-    BB = spm_vol(series1(1,n)); B.fname = [pwd filesep 'between_pairs_difference.nii'];
-    Maps{2} = spm_write_vol(B,Bimg); clear BB Bimg img
+    BB = spm_vol(series1(1,:)); BB.fname = [pwd filesep 'between_pairs_difference.nii'];
+    Maps{2} = spm_write_vol(BB,Bimg); clear BB Bimg img
 end
     
 
@@ -101,10 +103,10 @@ if nargout == 4
    disp('Computing the stats')
    if ~isempty(covariate)
        disp('adjusting data for the covariate')
-       B = pinv(covariate)*(W-mean(W));
-       W = W-(covariate*B);
-       B = pinv(covariate)*(B-mean(B));
-       B = B-(covariate*B);
+       b = pinv(covariate)*(W-mean(W));
+       W = W-(covariate*b);
+       b = pinv(covariate)*(B-mean(B));
+       B = B-(covariate*b);
    end
    
    Stats.means_within    = mean(W);
@@ -116,8 +118,8 @@ if nargout == 4
    nboot = 600; alphav = 5/100;
    low = round((alphav*nboot)/2); high = nboot - low;
    WP = W(:,1); WP = WP(randi(size(W,1),size(W,1),nboot));
-   BP = B(:,1); BP = BP(randi(size(B,1),size(B,1),nboot));
    WC = W(:,2); WC = WC(randi(size(W,2),size(W,2),nboot));
+   BP = B(:,1); BP = BP(randi(size(B,1),size(B,1),nboot));
    BC = B(:,2); BC = BC(randi(size(B,2),size(B,2),nboot));
    
    % mean 
@@ -131,15 +133,21 @@ if nargout == 4
    Stats.CI.means_between(2,:) = [MBP(high) MBP(high)];
    clear MBP MBC
    
-   DP = sort(mean(WP,1)-mean(BP,1)); 
-   DC = sort(mean(WC,1)-mean(BC,1)); 
-   Stats.means_difference = mean(W)-mean(B);
+   DP = sort(mean(WP,1))-sort(mean(BP,1)); 
+   DC = sort(mean(WC,1))-sort(mean(BC,1)); 
+   Stats.means_difference = mean(W,1)-mean(B,1);
    Stats.CI.means_difference(1,:) = [DP(low)  DC(low)];
    Stats.CI.means_difference(2,:) = [DP(high) DC(high)];
-   pb = sum(DP > 0) / nboot; 
-   Stats.pval.means_difference = 2*min(pb,1-pb);
-   pb = sum(DC > 0) / nboot; 
+   pb = length(find(DP>0)) / nboot; 
+   Stats.pval.means_difference(1) = 2*min(pb,1-pb);
+   if Stats.pval.means_difference(1) == 0
+       Stats.pval.means_difference(1) = 1/nboot;
+   end
+   pb = length(find(DC>0)) / nboot; 
    Stats.pval.means_difference(2) = 2*min(pb,1-pb);
+   if Stats.pval.means_difference(2) == 0
+       Stats.pval.means_difference(2) = 1/nboot;
+   end
    clear DP DC
   
    % median 
@@ -158,10 +166,15 @@ if nargout == 4
    Stats.medians_difference = median(W)-median(B);
    Stats.CI.medians_difference(1,:) = [DP(low)  DC(low)];
    Stats.CI.medians_difference(2,:) = [DP(high) DC(high)];
-   pb = sum(DP > 0) / nboot; 
+   pb = length(find(DP>0)) / nboot; 
    Stats.pval.medians_difference = 2*min(pb,1-pb);
-   pb = sum(DC > 0) / nboot; 
+   if Stats.pval.medians_difference(1) == 0
+       Stats.pval.medians_difference(1) = 1/nboot;
+   end
    Stats.pval.medians_difference(2) = 2*min(pb,1-pb);
+   if Stats.pval.medians_difference(2) == 0
+       Stats.pval.medians_difference(2) = 1/nboot;
+   end
    clear DP DC   
    
 end
