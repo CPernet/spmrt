7e8567ace46be218033a1d8bf63d7542d1ca777c
Cyril Pernet <cyril.pernet@ed.ac.uk>
2017-09-01 13:26:22 +0100

    data viz for spmrt_corrsub

--- spmrt_tissue_reliability.m
+++ spmrt_tissue_reliability.m
@@ -3,7 +3,7 @@ function varargout = spmrt_tissue_reliability(varargin)
 % code for spmrt_tissue_reliability.fig, a simple interface to show
 % reliability/correlation curves per tissue type whilst plotting the data 
 % from initial images with an interactive thresholding of tissue maps
-% Last Modified by GUIDE v2.5 20-Jul-2017 17:18:56
+% Last Modified by GUIDE v2.5 31-Aug-2017 11:25:23
 % See also: GUIDE, GUIDATA, GUIHANDLES
 %
 % FORMAT varargout = spmrt_tissue_reliability(Data,GM,WM,CSF)
@@ -41,28 +41,41 @@ else
 end
 % End initialization code - DO NOT EDIT
 
-
+% warning off
 % --- Executes just before spmrt_tissue_reliability is made visible.
-function spmrt_tissue_reliability_OpeningFcn(hObject, eventdata, handles, varargin)
+function spmrt_tissue_reliability_OpeningFcn(hObject, ~, handles, varargin)
 global handles
 
-handles.data= varargin{1};
-handles.GM  = varargin{2};
-handles.WM  = varargin{3};
-handles.CSF = varargin{4};
-handles.output = hObject;
-set(hObject,'Tag','Tissue type reliability');
-guidata(hObject,handles);
+handles.data    = varargin{1};
+handles.GM      = varargin{2};
+handles.WM      = varargin{3};
+handles.CSF     = varargin{4};
+handles.showGM  = 1;
+handles.showWM  = 1;
+handles.showCSF = 1;
+handles.output  = hObject;
 
-% UIWAIT makes spmrt_tissue_reliability wait for user response (see UIRESUME)
-% uiwait(handles.figure1);
+set(hObject,'Tag','Tissue type reliability');
+axes2_CreateFcn(hObject, [], handles)
+guidata(hObject,handles);
+uiwait(handles.output);
+
 
 % --- Outputs from this function are returned to the command line.
 function varargout = spmrt_tissue_reliability_OutputFcn(hObject, eventdata, handles) 
-varargout{1} = handles.output;
+if isempty(handles)
+    varargout{1} = 'Reliability data viz terminated';
+else
+    varargout{1} = handles;
+    delete(handles.output)
+end
 
 
+% --- Executes during object creation, after setting all properties.
+
+% -------------------------------------------------
 %% show the correlations
+% -------------------------------------------------
 
 function curves_title_Callback(hObject, eventdata, handles)
 function curves_title_CreateFcn(hObject, eventdata, handles)
@@ -77,20 +90,22 @@ global handles
 l = size(handles.data,2);
 plot(handles.data(2,:),'k','LineWidth',1)
 hold on; fillhandle=fill([1:l,l:-1:1],[handles.data(1,:),fliplr(handles.data(3,:))],[0 0 1]);
-set(fillhandle,'EdgeColor',[0 0 1],'FaceAlpha',0.2,'EdgeAlpha',0.8);%set edge color
+set(fillhandle,'EdgeColor',[0 0 1],'FaceAlpha',0.2,'EdgeAlpha',0.8); 
 
 plot(handles.data(5,:),'k','LineWidth',1)
 hold on; fillhandle=fill([1:l,l:-1:1],[handles.data(4,:),fliplr(handles.data(6,:))],[1 0 0]);
-set(fillhandle,'EdgeColor',[1 0 0],'FaceAlpha',0.2,'EdgeAlpha',0.8);%set edge color
+set(fillhandle,'EdgeColor',[1 0 0],'FaceAlpha',0.2,'EdgeAlpha',0.8); 
 
 plot(handles.data(8,:),'k','LineWidth',1)
 hold on; fillhandle=fill([1:l,l:-1:1],[handles.data(7,:),fliplr(handles.data(9,:))],[0 1 0 ]);
-set(fillhandle,'EdgeColor',[0 1 0],'FaceAlpha',0.2,'EdgeAlpha',0.8);%set edge color
+set(fillhandle,'EdgeColor',[0 1 0],'FaceAlpha',0.2,'EdgeAlpha',0.8); 
 
+grid on; box on
 guidata(hObject,handles);
 
+% -------------------------------------------------
 %% show the data and update with button and sliders
-
+% -------------------------------------------------
 
 function voxel_title_Callback(hObject, eventdata, handles)
 function voxel_title_CreateFcn(hObject, eventdata, handles)
@@ -98,13 +113,31 @@ if ispc && isequal(get(hObject,'BackgroundColor'), get(0,'defaultUicontrolBackgr
     set(hObject,'BackgroundColor','white');
 end
 
-% --- Executes during object creation, after setting all properties.
 function axes1_CreateFcn(hObject, eventdata, handles)
 global handles
 
-scatter(handles.GM(:,[1 2]),50,[0 0 1]); hold on
-scatter(handles.WM(:,[1 2]),50,[1 0 0]);
-scatter(handles.CSF(:,[1 2]),50,[0 1 0]);
+if handles.showCSF  == 1
+    s = scatter(handles.CSF(:,1),handles.CSF(:,2),50,[0 1 0]); hold on
+    s.MarkerEdgeAlpha = 0.5;
+    s.MarkerFaceAlpha = 0.5;
+    % s.MarkerFaceColor = 'green';
+end
+
+if handles.showWM  == 1
+    s = scatter(handles.WM(:,1), handles.WM(:,2) ,50,[1 0 0]); hold on
+    s.MarkerEdgeAlpha = 0.8;
+    s.MarkerFaceAlpha = 0.8;
+    % s.MarkerFaceColor = 'red';
+end
+
+if handles.showGM  == 1
+    s = scatter(handles.GM(:,1), handles.GM(:,2) ,50,[0 0 1]); hold on
+    s.MarkerEdgeAlpha = 0.8;
+    s.MarkerFaceAlpha = 0.8;
+    % s.MarkerFaceColor = 'blue';
+end
+
+grid on; box on
 guidata(hObject,handles);
 
 % --- Executes on button press in display_GM_voxels.
@@ -114,7 +147,14 @@ function display_GM_voxels_Callback(hObject, eventdata, handles)
 % handles    structure with handles and user data (see GUIDATA)
 
 content = get(hObject,'Value');
-
+if content == 1
+    handles.showGM = 1;
+    guidata(hObject,handles);
+    axes1_CreateFcn(hObject, eventdata, handles)
+else
+    handles.showGM = 0;
+end
+guidata(hObject,handles);
 
 % --- Executes on button press in display_WM_voxels.
 function display_WM_voxels_Callback(hObject, eventdata, handles)
@@ -179,5 +219,3 @@ function CSF_slider_Callback(hObject, eventdata, handles)
 
 % Hints: get(hObject,'Value') returns position of slider
 %        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
-
-
